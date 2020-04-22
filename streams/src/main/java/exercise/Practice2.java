79dbed05d3b642c9fc32e9350b77307528ffa583
sita1 <sitasharma853@gmail.com>
2020-04-22 22:46:56 +0530

    IntStream examples

--- streams/src/main/java/exercise/Practice2.java
+++ streams/src/main/java/exercise/Practice2.java
@@ -1,8 +1,12 @@
 package exercise;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
+
+import mockData.MockData;
+import streams.Person;
 
 // find persons age less or equal 18 
 // and another method to find first 10 person whose age less than 18
@@ -17,6 +21,27 @@ public class Practice2 {
 		System.out.println("inclusive through range method");
 		IntStream.rangeClosed(0, 10).forEach(System.out::println);
 	}
+	
+
+    @Test
+    public void rangeIteratingLists() throws Exception {
+      List<Person> people = MockData.getPeople();
+      
+      IntStream.range(0, people.size()).forEach(index->{
+    	  Person person = people.get(index);
+    	  System.out.print(person);
+    	  });
+     
+    }
+    
+    @Test
+    public void printConsecutiveIntegers() throws Exception {
+    IntStream.iterate(0, operand->operand+1).filter(i->i%2==0).limit(20).forEach(System.out::println);
+      
+    
+     
+    }
+	
 	
 	
 }
