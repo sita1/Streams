a88efb327d034609ad1ec17b1797922f95c85ab3
sita1 <sitasharma853@gmail.com>
2020-04-23 00:00:28 +0530

    distict

---
+++ streams/src/main/java/exercise/Practice4.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableList;
+
+public class Practice4 {
+	
+	@Test
+	public void distictData() throws Exception {
+		final List<Integer> numbers = ImmutableList.of(1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5, 5,6,6,6,6,6, 2, 3, 4, 7);
+		numbers.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
+	}
+
+}
